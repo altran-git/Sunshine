e0c5200e0fd677337214e1c95db615f39d81a42c
Andy Tran <Andy Tran>
2016-04-28 00:45:20 -0700

    Add Launch SettingsActivity, Lesson 3 step 16

--- app/src/main/java/com/a2g/nd/sunshine/DetailActivity.java
+++ app/src/main/java/com/a2g/nd/sunshine/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.a2g.nd.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,6 +18,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +46,8 @@ public class DetailActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 

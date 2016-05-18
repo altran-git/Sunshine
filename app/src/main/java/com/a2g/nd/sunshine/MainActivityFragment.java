b80be09be2cb5be6691cdc0927d1444e281473cb
Andy Tran <Andy Tran>
2016-05-17 23:51:40 -0700

    Added Test Code and WeatherContract,WeatherDbHelper from Lesson 4

--- app/src/main/java/com/a2g/nd/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/a2g/nd/sunshine/MainActivityFragment.java
@@ -37,6 +37,8 @@ import java.util.ArrayList;
  */
 public class MainActivityFragment extends Fragment {
 
+    public final String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
     ArrayAdapter<String> mForecastAdapter;
 
     public MainActivityFragment() {
@@ -45,12 +47,14 @@ public class MainActivityFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "onCreate");
         // Add this line in order for this fragment to handle menu events.
         setHasOptionsMenu(true);
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        Log.d(LOG_TAG, "onCreateOptionsMenu");
         // Inflate the fragment menu
         inflater.inflate(R.menu.forecastfragment, menu);
     }
@@ -77,10 +81,37 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
     public void onStart() {
         super.onStart();
+        Log.d(LOG_TAG, "onStart");
         updateWeather();
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
